6907f255c3e66a2530bd7b92bcad4e6f5dd478dc
Rex Douglas <ademoss@unca.edu>
2023-08-25 05:17:42 -0400

    Project Start

---
+++ index.js
@@ -0,0 +1,77 @@
+import createPrompt from 'prompt-sync';
+let prompt = createPrompt();
+
+let employee = {}
+
+let firstName = prompt("First Name: ");
+if (!firstName) {
+    console.error('Invalid first name');
+    process.exit(1);
+}
+
+employee.firstName = firstName;
+
+let lastName = prompt("Last Name: ");
+if(!lastName) {
+    console.error(`Invalid last name`);
+    process.exit(1);
+}
+
+employee.lastName = lastName;
+
+let startDateYear = prompt("Employee Start Year (1990 - 2023): ");
+startDateYear = Number(startDateYear);
+// Check if it is a valid integer
+if(!Number.isInteger(startDateYear)) {
+    console.error('Enter a valid start date year');
+    process.exit(1);
+}
+// Check if the number is in the range
+if(startDateYear < 1990 || startDateYear > 2023) {
+    console.error('Enter a valid start date year within the correct range');
+    process.exit(1);
+}
+
+ let startDateMonth = prompt("employee Start Date Month (1-12): ");
+ startDateMonth = Number(startDateMonth);
+ // Check if it is a valid integer
+ if (!Number.isInteger(startDateMonth)) {
+    console.error('Enter a valid start date month');
+    process.exit(1);
+ }
+ //check if the number is in the range
+ if(startDateMonth < 1 || startDateMonth > 12) {
+    console.error('Enter a start date month within the correct range');
+    process.exit(1);
+ }
+
+ let startDateDay = prompt("Employee Start Date Day (1 - 31): ");
+ startDateDay = Number (startDateDay);
+ //check if it is a valid integer
+if(!Number.isInteger(startDateDay)){
+    consoleerror('Enter a valid start date day');
+    process.exit(1);
+}
+//check if the number is in range
+if(startDateDay < 1 || startDateDay > 31){
+    console.error('Enter a start date day within the correct range');
+    process.exit(1);
+}
+
+// Date elements are correct, lets create the date
+employee.startDateDay = new Date(startDateYear, startDateMonth - 1, startDateDay);
+
+let isActive = prompt("Is employee active (yes or no): ");
+
+//Check if incorrect value was entered
+if (isActive !== "yes" && isActive !== "no") {
+    console.error('Enter a yes or no for employee active stats');
+    process.exit(1);
+}
+
+employee.isActive = (isActive === "yes");
+
+// Output Employee JSON
+
+const json = JSON.stringify(employee, null, 2);
+console.log(`Employee: ${json}`);
